29186a321d972e2391d1c415369eaf12f9b63981
Juan Fernando Salazar Serrano <juanfernandosalazarserrano@gmail.com>
2025-11-28 22:13:41 -0500

    first commit

---
+++ backend/springboot-backend-chat/src/main/java/com/vmsecca/springboot/backend/chat/SpringbootBackendChatApplication.java
@@ -0,0 +1,13 @@
+package com.vmsecca.springboot.backend.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackendChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackendChatApplication.class, args);
+	}
+
+}
