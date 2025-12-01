96d5d637cd7a4a96b5c4c5c30ff70cedd8476bf1
Juan Fernando Salazar Serrano <juanfernandosalazarserrano@gmail.com>
2025-11-30 19:13:01 -0500

    Login front

--- frontend/vmsecca/src/app/app.routes.ts
+++ frontend/vmsecca/src/app/app.routes.ts
@@ -1,11 +1,15 @@
 import { Routes } from '@angular/router';
 import { Chat } from './components/chat';
+import { Login } from './components/login/login';
 
 export const routes: Routes = [
 
   {path: '', redirectTo: 'chat', pathMatch: 'full'}, // default route
 
-  {path: 'chat', component: Chat}
+  {path: 'chat', component: Chat},
+
+  {path: 'login', component: Login}
+
 
 
 ];
